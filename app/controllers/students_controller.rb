95fae5ce4170de0cf13eb734070d9104c33885d0
isaac <isaactonyloi@gmail.com>
2023-03-16 14:59:51 +0300

    dynamic routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  def search
+    @students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+
+  end
+
 end
